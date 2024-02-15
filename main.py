ab560a9f5b229d8cebfb1dfeef0f7d4177562f72
Mukhametkhan <topinambur86@icloud.com>
2024-02-15 10:45:51 +0500

    Created new project

---
+++ main.py
@@ -0,0 +1,17 @@
+import calculator
+
+
+if __name__ == '__main__':
+    calc = calculator.Calculator()
+    calc.add_car(
+        calculator.Car("Toyota Corolla", price = 110000, fuel_economy=7, service_cost= 1200, insurance_cost= 2500),
+    )
+    calc.add_car(
+        calculator.ElectricCar("Tesla Model 3", 200000, 5500, 150),
+    )
+    calc.add_car(
+        calculator.Car("Range Rover", 650000, 3,
+                       service_cost=3000, insurance_cost= 2500),
+    )
+    calc.print_cars()
+
